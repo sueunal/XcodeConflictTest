4b2f2fdc1625331e78ad917d43a146af73b361a6
sueun kim <doms@kakao.com>
2024-08-03 16:08:34 +0900

    [init]: init commit

---
+++ XcodeConflictTest/XcodeConflictTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeConflictTest
+//
+//  Created by Soom on 8/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeConflictTest/XcodeConflictTest/XcodeConflictTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeConflictTestApp.swift
+//  XcodeConflictTest
+//
+//  Created by Soom on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeConflictTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
