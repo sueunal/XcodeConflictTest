72fd4dd0b2fe56380ca2bfc18c9de5a90d604079
sueun kim <doms@kakao.com>
2024-08-03 16:20:22 +0900

    [add]: 메인에서 수정

--- XcodeConflictTest/XcodeConflictTest/CreatTest0.swift
+++ XcodeConflictTest/XcodeConflictTest/CreatTest0.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct CreatTest0: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("메인에서 수정함")
+        }
     }
 }
 
