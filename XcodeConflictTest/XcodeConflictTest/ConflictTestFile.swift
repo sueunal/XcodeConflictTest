19f800447dfca7e1a6906d148e037fccf558083f
sueun kim <doms@kakao.com>
2024-08-03 16:16:18 +0900

    [add]: test0 create file

---
+++ XcodeConflictTest/XcodeConflictTest/ConflictTestFile.swift
@@ -0,0 +1,18 @@
+//
+//  ConflictTestFile.swift
+//  XcodeConflictTest
+//
+//  Created by Soom on 8/3/24.
+//
+
+import SwiftUI
+
+struct ConflictTestFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConflictTestFile()
+}
---
+++ XcodeConflictTest/XcodeConflictTest/CreatTest0.swift
@@ -0,0 +1,18 @@
+//
+//  CreatTest0.swift
+//  XcodeConflictTest
+//
+//  Created by Soom on 8/3/24.
+//
+
+import SwiftUI
+
+struct CreatTest0: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreatTest0()
+}
